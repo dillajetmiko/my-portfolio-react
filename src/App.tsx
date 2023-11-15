3f0e174fa152a48bcf94e523e6c6cf179d7ff6dd
Dilla Jetmiko <dilla.jetmiko@gmail.com>
2023-11-15 14:20:04 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
+  // useEffect(() => {
+  //   window.location.href = "https://portofolio-dilla.000webhostapp.com/";
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      dilla
     </div>
   );
 }
